e6d3422648d8a3258a85b5a15fff082c67aab355
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2025-04-15 15:18:30 +0800

    Bug fix in TupleHashHelper.hpp

--- src/utils/AtomicInfoHelper.cpp
+++ src/utils/AtomicInfoHelper.cpp
@@ -1,9 +1,12 @@
 #include "AtomicInfoHelper.hpp"
 
+#include <cmath>
+
 double AtomicInfoHelper::GausModelFunction(double * x, double * par)
 {
+    double pi{ 3.14159265358979323846264338327950288 };
     double tau_square{ par[1]*par[1] };
-    double y{ par[0] * pow(2.0*M_PI*tau_square, -1.5) * exp(-x[0]*x[0]/(2.0*tau_square)) };
+    double y{ par[0] * std::pow(2.0*pi*tau_square, -1.5) * std::exp(-x[0]*x[0]/(2.0*tau_square)) };
     return y;
 }
 
