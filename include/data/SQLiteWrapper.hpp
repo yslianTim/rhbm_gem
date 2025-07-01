3f73e7f3263ae1c67cd43f6e9976fd5024e6c611
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2025-07-01 14:24:58 +0800

    Reorganize SQLiteWrapper class

--- include/data/SQLiteWrapper.hpp
+++ include/data/SQLiteWrapper.hpp
@@ -99,23 +99,94 @@ public:
     };
 
 public:
-    explicit SQLiteWrapper(const std::filesystem::path & database_path);
-    ~SQLiteWrapper();
+    explicit SQLiteWrapper(const std::filesystem::path & database_path) :
+        m_database_ptr{ nullptr }, m_statement_ptr{ nullptr }
+    {
+    #ifdef _WIN32
+        std::wstring wpath{ database_path.wstring() };
+        auto return_code{ sqlite3_open16(wpath.c_str(), &m_database_ptr) };
+    #else
+        std::string path_utf8{ database_path.u8string() };
+        auto return_code{ sqlite3_open_v2(path_utf8.c_str(), &m_database_ptr,
+                                        SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE,
+                                        nullptr) };
+    #endif
+        if (return_code != SQLITE_OK)
+        {
+            sqlite3_close(m_database_ptr);
+            m_database_ptr = nullptr;
+            throw std::runtime_error("Cannot open database: " + ErrorMessage());
+        }
+    }
+
+    ~SQLiteWrapper()
+    {
+        if (m_statement_ptr)
+        {
+            Finalize();
+        }
+        if (m_database_ptr)
+        {
+            sqlite3_close(m_database_ptr);
+            m_database_ptr = nullptr;
+        }
+    }
 
     SQLiteWrapper(const SQLiteWrapper &) = delete;
     SQLiteWrapper & operator=(const SQLiteWrapper &) = delete;
 
     static int StepDone(void) { return SQLITE_DONE; }
     static int StepRow(void) { return SQLITE_ROW; }
-
-    void Execute(const std::string & sql);
-    void Prepare(const std::string & sql);
-    int StepNext(void) { return sqlite3_step(m_statement_ptr); }
-    void StepOnce(void);
     void Reset(void) { sqlite3_reset(m_statement_ptr); }
-    void Finalize(void);
+    int StepNext(void) { return sqlite3_step(m_statement_ptr); }
     std::string ErrorMessage(void) const { return std::string(sqlite3_errmsg(m_database_ptr)); }
-    void ClearTable(const std::string & table_name);
+
+    void Execute(const std::string & sql)
+    {
+        char * error_message{ nullptr };
+        auto return_code{ sqlite3_exec(m_database_ptr, sql.c_str(), nullptr, nullptr, &error_message) };
+        if (return_code != SQLITE_OK)
+        {
+            std::string msg{ (error_message ? error_message : "Unknown error") };
+            sqlite3_free(error_message);
+            throw std::runtime_error("Execute SQL failed: " + msg);
+        }
+    }
+
+    void Prepare(const std::string & sql)
+    {
+        Finalize();
+        auto return_code{ sqlite3_prepare_v2(m_database_ptr, sql.c_str(), -1, &m_statement_ptr, nullptr) };
+        if (return_code != SQLITE_OK)
+        {
+            throw std::runtime_error("Prepared failed: " + ErrorMessage());
+        }
+    }
+
+    void StepOnce(void)
+    {
+        auto return_code{ sqlite3_step(m_statement_ptr) };
+        if (return_code != SQLITE_DONE)
+        {
+            Finalize();
+            throw std::runtime_error("Statement step failed: " + ErrorMessage());
+        }
+    }
+    
+    void Finalize(void)
+    {
+        if (m_statement_ptr)
+        {
+            sqlite3_finalize(m_statement_ptr);
+        }
+        m_statement_ptr = nullptr;
+    }
+
+    void ClearTable(const std::string & table_name)
+    {
+        std::string sql{ "DELETE FROM " + table_name + ";" };
+        this->Execute(sql);
+    }
 
     template<typename... Ts>
     std::vector<std::tuple<Ts...>> Query(const std::string & sql)
