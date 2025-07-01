45f75949550f6a35085e136ae6daf1a707b39bcd
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2025-07-01 14:49:05 +0800

    Remove unused specialization data type in SQLiteBinder

--- include/data/SQLiteBinder.hpp
+++ include/data/SQLiteBinder.hpp
@@ -124,36 +124,6 @@ struct SQLiteBinder<std::vector<std::tuple<float, float>>>
     }
 };
 
-// std::vector<std::tuple<float, float, float>> specialization
-template<>
-struct SQLiteBinder<std::vector<std::tuple<float, float, float>>>
-{
-    static int Bind(sqlite3_stmt * stmt, int index, const std::vector<std::tuple<float, float, float>> & value)
-    {
-        if(value.empty())
-        {
-            return sqlite3_bind_blob(stmt, index, nullptr, 0, SQLITE_STATIC);
-        }
-        
-        std::vector<float> contiguous;
-        contiguous.reserve(value.size() * 3);
-        for(const auto & tup : value)
-        {
-            contiguous.push_back(std::get<0>(tup));
-            contiguous.push_back(std::get<1>(tup));
-            contiguous.push_back(std::get<2>(tup));
-        }
-        
-        return sqlite3_bind_blob(
-            stmt,
-            index,
-            reinterpret_cast<const void*>(contiguous.data()),
-            static_cast<int>(contiguous.size() * sizeof(float)),
-            SQLITE_TRANSIENT
-        );
-    }
-};
-
 // std::vector<std::tuple<double, double>> specialization
 template<>
 struct SQLiteBinder<std::vector<std::tuple<double, double>>>
