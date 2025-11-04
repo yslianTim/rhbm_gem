01c5d0891fab4d3435addf2864b6c874a5ac7ce3
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2025-11-04 22:38:53 +0800

    Add HRLModelTester

---
+++ include/utils/HRLModelTester.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <tuple>
+#include <Eigen/Dense>
+
+class HRLModelTester
+{
+    std::vector<std::tuple<std::vector<Eigen::VectorXd>, std::string>> m_data_array;
+
+public:
+    HRLModelTester(void) = delete;
+    explicit HRLModelTester(int basis_size, int member_size);
+    ~HRLModelTester() = default;
+
+    void BuildDataArray(void);
+
+private:
+
+};
---
+++ src/utils/HRLModelTester.cpp
@@ -0,0 +1,14 @@
+#include "HRLModelTester.hpp"
+#include "HRLModelHelper.hpp"
+#include "Logger.hpp"
+
+HRLModelTester::HRLModelTester(int basis_size, int member_size) :
+    m_data_array{}
+{
+
+}
+
+void HRLModelTester::BuildDataArray(void)
+{
+    m_data_array.clear();
+}
