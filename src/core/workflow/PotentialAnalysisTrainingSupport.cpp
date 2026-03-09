b215664bbbe03e35eed7291ab84b61dbec750bae
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2026-03-09 17:39:29 +0800

    Minor update

--- src/core/workflow/PotentialAnalysisTrainingSupport.cpp
+++ src/core/workflow/PotentialAnalysisTrainingSupport.cpp
@@ -10,7 +10,7 @@ namespace rhbm_gem::detail {
 
 std::vector<double> BuildOrderedAlphaRTrainingList()
 {
-    std::vector<double> alpha_list{ 0.0, 0.1, 0.2, 0.3, 0.4, 0.5 };
+    std::vector<double> alpha_list{ 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
     std::sort(alpha_list.begin(), alpha_list.end());
     return alpha_list;
 }
