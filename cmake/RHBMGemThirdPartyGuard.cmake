6e62c2b303c93f989dbfed8c157c542d0e63f8b1
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2026-03-10 12:23:20 +0800

    Remove unused cmake file

--- cmake/RHBMGemThirdPartyGuard.cmake
+++
@@ -1,9 +0,0 @@
-function(rhbm_gem_require_bundled_dependency dependency_name check_path)
-    set(_dependency_path "${PROJECT_SOURCE_DIR}/${check_path}")
-    if(NOT EXISTS "${_dependency_path}")
-        message(FATAL_ERROR
-            "Bundled ${dependency_name} is required but '${check_path}' is missing.\n"
-            "Restore the bundled dependency source under third_party/, or install\n"
-            "the system package and configure with -DUSE_SYSTEM_LIBS=ON.")
-    endif()
-endfunction()
