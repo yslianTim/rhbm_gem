648d44534d9396669385bf15374e40d93044c3f4
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2026-03-25 10:40:57 +0800

    Update 00_quickstart.sh

--- resources/examples/cli/00_quickstart.sh
+++ resources/examples/cli/00_quickstart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Find executable
+executable="./build/bin/RHBM-GEM"
+
 # Define working directory (Require user input)
 work_dir=$1 # Check if work_dir is provided
 if [ -z "$work_dir" ]; then
@@ -9,6 +12,7 @@ fi
 
 mkdir -p "$work_dir"
 echo "Working directory: $work_dir"
+ln -s $executable "$work_dir/RHBM-GEM"
 cd "$work_dir" || { echo "Error: Failed to enter directory $work_dir"; exit 1; }
 
 # Create data/ folder under work_dir and download required model/map files
@@ -23,15 +27,15 @@ gunzip emd_11103_additional.map.gz
 
 # Run RHBM-GEM
 cd "$work_dir" || { echo "Error: Failed to enter directory $work_dir"; exit 1; }
-executable="./build/bin/RHBM-GEM"
+
 database="$data_dir/database.sqlite"
 alpha_train=false
 jobs=4
 key_tag="6z6u"
 map_name="$data_dir/emd_11103_additional.map"
 model_name="$data_dir/6Z6U.cif"
-$executable potential_analysis -d $database -m $map_name -a $model_name -k $key_tag -j $jobs --training-alpha $alpha_train
+$work_dir/RHBM-GEM potential_analysis -d $database -m $map_name -a $model_name -k $key_tag -j $jobs --training-alpha $alpha_train
 
 # Run Gaus Estimation Result Dumping
 echo "Output gaussian estimation results of following model: $key_tag"
-$executable result_dump -o $work_dir -d $database -k $key_tag -p 2
+$work_dir/RHBM-GEM result_dump -o $work_dir -d $database -k $key_tag -p 2
