ea61d498231835b6dfb10b4c898f82d4083f7310
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2026-04-01 15:05:24 +0800

    Minor update

--- src/data/io/sqlite/ModelObjectStorage.hpp
+++ src/data/io/sqlite/ModelObjectStorage.hpp
@@ -8,11 +8,13 @@ namespace rhbm_gem {
 class SQLiteWrapper;
 class ModelObject;
 
-class ModelObjectStorage {
-  public:
+class ModelObjectStorage
+{
+public:
     static void CreateTables(SQLiteWrapper& database);
     static void Save(SQLiteWrapper& database, const ModelObject& obj, const std::string& key_tag);
     static std::unique_ptr<ModelObject> Load(SQLiteWrapper& database, const std::string& key_tag);
+    
 };
 
 } // namespace rhbm_gem
