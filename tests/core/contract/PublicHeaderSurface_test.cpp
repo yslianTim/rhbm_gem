f36742efc935fd815b467575b2abde40eb58b704
Yu-Hsiang Lien <timfrogmelon@gmail.com>
2026-05-07 15:21:57 +0800

    Remove PublicHeaderSurface_test.cpp

--- tests/core/contract/PublicHeaderSurface_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <rhbm_gem/core/command/CommandTypes.hpp>
-
-TEST(PublicHeaderSurfaceTest, CommandApiExposesStableDefaultDatabasePathHelper) {
-    const auto default_path{ rhbm_gem::GetDefaultDatabasePath() };
-    EXPECT_FALSE(default_path.empty());
-    EXPECT_EQ(default_path.filename(), "database.sqlite");
-}
